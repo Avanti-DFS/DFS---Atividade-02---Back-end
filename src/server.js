dc6ecba186df9f7bf80285d1ebecee5f2afb40e3
Felipe <felipe21fab@gmail.com>
2024-05-03 15:49:06 -0300

    Add authorization and authenticate to routes, Add table Cliente to schema, Add CLiente controller and routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@
 import express from "express";
 import { routes as localRoutes } from "./routes/LocalRoutes.js";
 import { routers as categoriasRoutes } from "./routes/categoriaRoutes.js";
+import { routes as clienteRoutes } from "./routes/ClienteRoutes.js"
+import { routes as loginRoutes } from "./routes/LoginRoutes.js"
 
 const app = express();
 
@@ -9,6 +11,8 @@ app.use(express.json());
 
 app.use(localRoutes);
 app.use(categoriasRoutes);
+app.use(clienteRoutes)
+app.use(loginRoutes)
 
 app.listen(3000, () => {
     console.log("Server running")
